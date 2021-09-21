cb2ede03518b41b464ea0c4d1cf3b956c05a495b
Pavel Tuzov <pavel.tuzov@gmail.com>
2021-09-21 15:14:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,8 +37,8 @@ resource "azurerm_app_service_plan" "demo" {
   resource_group_name = azurerm_resource_group.demo.name
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "Dynamic"
+    size = "Y1"
   }
 }
 
